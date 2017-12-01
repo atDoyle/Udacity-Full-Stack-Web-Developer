6ee3f04324ec06b069b3a2016ee68461243f1919
atDoyle <Doyle602@gmail.com>
2017-12-01 15:08:38 -0500

    Add files via upload

---
+++ logs_analysis/get_articles.py
@@ -0,0 +1,42 @@
+import psycopg2
+
+
+DBNAME = "news"
+
+
+def get_articles():
+    """Return the 3 most read articles."""
+    sql = ("select articles.title as article, count(*) as views "
+           "from (articles inner join log on log.path "
+           "like concat('%', articles.slug)) "
+           "where log.status = '200 OK' "
+           "group by articles.title "
+           "order by views desc limit 3;"
+           )
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(sql)
+    articles = c.fetchall()
+    colnames = [desc[0] for desc in c.description]
+    db.close()
+    return colnames, articles
+
+
+def nice_print(results):
+    """Print in a nice way."""
+    fh = open('get_articles.txt', 'w')
+    fh.write(results[0][0].title() + ' | ' + results[0][1].title())
+    print results[0][0].title(), "|", results[0][1].title()
+    for i in range(len(results[1])):
+        print '{} | {:,d} Views'.format(
+            results[1][i][0],
+            results[1][i][1]
+            )
+        fh.write('\n{} | {:,d} Views'.format(
+            results[1][i][0],
+            results[1][i][1]
+            ))
+    fh.close()
+
+
+nice_print(get_articles())
---
+++ logs_analysis/get_authors.py
@@ -0,0 +1,42 @@
+import psycopg2
+
+
+DBNAME = "news"
+
+
+def get_authors():
+    """Return the most popular authors."""
+    sql = ("select authors.name as author, count(*) as views "
+           "from (authors inner join articles "
+           "on (authors.id = articles.author) inner join log "
+           "on log.path "
+           "like concat('%', articles.slug)) "
+           "where log.status = '200 OK' "
+           "group by authors.name order by views desc;"
+           )
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(sql)
+    authors = c.fetchall()
+    colnames = [desc[0] for desc in c.description]
+    db.close()
+    return colnames, authors
+
+
+def nice_print(results):
+    """Print in a nice way."""
+    fh = open('get_authors.txt', 'w')
+    fh.write(results[0][0].title() + ' | ' + results[0][1].title())
+    print results[0][0].title(), "|", results[0][1].title()
+    for i in range(len(results[1])):
+        print '{} | {:,d} Views'.format(
+            results[1][i][0],
+            results[1][i][1]
+            )
+        fh.write('\n{} | {:,d} Views'.format(
+            results[1][i][0],
+            results[1][i][1]
+            ))
+    fh.close()
+
+nice_print(get_authors())
